8ad540327d062d021a86510c0a5c52c8d48cd7ec
dhjCoding <92060457+dhjCoding@users.noreply.github.com>
2022-07-04 21:48:40 +0200

    Create Simple_DialogBox.java
    Simple Java Dialog Box

---
+++ Simple_DialogBox.java
@@ -0,0 +1,15 @@
+
+package simple_dialogbox;
+
+import javax.swing.JOptionPane;
+
+
+public class Simple_DialogBox {
+
+    
+    public static void main(String[] args) 
+    {
+        JOptionPane.showMessageDialog(null, "First Java Simple Java Dialog");
+    }
+    
+}
